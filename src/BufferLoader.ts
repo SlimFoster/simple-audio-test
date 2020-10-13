285fa9dc13270026ad6367c9b94298a15afa53c7
Adam Foster <slimfoster@gmail.com>
2020-10-12 22:57:47 -0500

    Update BufferLoader.ts

--- src/BufferLoader.ts
+++ src/BufferLoader.ts
@@ -1,61 +1,68 @@
 export class BufferLoader {
   private context: AudioContext;
   private urlList: string[];
-  private onload: (bufferList: AudioBuffer[]) => void;
+  private resolve: (bufferList: AudioBuffer[]) => void;
+  private reject: (reason?: any) => void;
   private bufferList: AudioBuffer[];
   private loadCount: number;
 
   constructor(
     context: AudioContext,
     urlList: string[],
-    callback: (buffer: AudioBuffer[]) => void
+    resolve: (buffer: AudioBuffer[]) => void,
+    reject: (reason?: any) => void
   ) {
     this.context = context;
     this.urlList = urlList;
-    this.onload = callback;
+    this.resolve = resolve;
+    this.reject = reject;
     this.bufferList = new Array();
     this.loadCount = 0;
+
+    this.doLoad();
   }
 
   public static load(context: AudioContext, urlList: string[]) {
     return new Promise<AudioBuffer[]>((resolve, reject) => {
-      new BufferLoader(context, urlList, resolve).doLoad(reject);
+      new BufferLoader(context, urlList, resolve, reject);
     });
   }
 
-  private doLoad (reject: (reason?: any) => void) {
+  private doLoad() {
     for (let i = 0; i < this.urlList.length; i++) {
-      const url = this.urlList[i];
-
-      // Load buffer asynchronously
-      const request = new XMLHttpRequest();
-      request.open("GET", url, true);
-      request.responseType = "arraybuffer";
-
-      request.onload = () => {
-        // Asynchronously decode the audio file data in request.response
-        this.context.decodeAudioData(
-          request.response,
-          (buffer) => {
-            if (!buffer) {
-              alert("error decoding file data: " + url);
-              return;
-            }
-            this.bufferList[i] = buffer;
-            if (++this.loadCount == this.urlList.length)
-              this.onload(this.bufferList);
-          },
-          (error) => {
-            reject("decodeAudioData error: " + error);
-          }
-        );
-      };
-
-      request.onerror = () => {
-        reject("XHR error loading url: " + url);
-      };
-
-      request.send();
+      this.loadBuffer(this.urlList[i], i);
     }
-  };
+  }
+
+  private loadBuffer(url: string, index: number) {
+    // Load buffer asynchronously
+    const request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.responseType = "arraybuffer";
+
+    request.onload = () => {
+      // Asynchronously decode the audio file data in request.response
+      this.context.decodeAudioData(
+        request.response,
+        (buffer) => {
+          if (!buffer) {
+            this.reject("error decoding file data: " + url);
+            return;
+          }
+          this.bufferList[index] = buffer;
+          if (++this.loadCount == this.urlList.length)
+            this.resolve(this.bufferList);
+        },
+        (error) => {
+          this.reject("decodeAudioData error: " + error);
+        }
+      );
+    };
+
+    request.onerror = () => {
+      this.reject("BufferLoader: XHR error loading " + url);
+    };
+
+    request.send();
+  }
 }
